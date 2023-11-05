221d509bea280faa7f7e8c0065b2f35d6ee19b03
JIAYUE11111 <1047881787@qq.com>
2023-11-06 00:26:38 +0800

    Update lex.cpp

--- lex.cpp
+++ lex.cpp
@@ -8,7 +8,7 @@ using namespace std;
 typedef struct Treenode {
 	char character;
 	Treenode*leftchild, *rightchild;
-}Treenode,*Tree;
+}Treenode, *Tree;
 typedef struct NFAnode
 {
 	char inputStatus1;
@@ -35,10 +35,17 @@ typedef struct StatusSetLine
 	bool flag;
 	bool isFinal;
 }StatusSetLine;
+typedef struct UpdateStatus
+{
+	vector<int>nextstatus;//存放输入字符串后的变化状态
+	vector<int>nowstatusindex;//存放当前对应状态的下标
+}UpdateStatus;
+
 vector<TransformState>TransformTable;//用于存储所有状态转化函数
 vector<TransformStateDFA>TransformTableDFA;//用于存储所有状态转化函数
 vector<StatusSetLine>StatusSets;//用与存放状态集
 vector<NFAnode*> StatusLine;//存放当前扫描得到的未标记状态
+vector<vector<int>>DFAState;//所有状态
 int StatusLineIndex = 0;//标记状态集里现在有几行
 
 char TEMP;//标记当前扫描的字符
@@ -50,11 +57,11 @@ char OperatorStackTop;
 Treenode *OperandsStackTop;
 int isoperator(char temp)//判断加入的是否为运算符
 {
-	if (temp == '(' || temp == ')' || temp == '|' || temp == '*'||temp=='&')
+	if (temp == '(' || temp == ')' || temp == '|' || temp == '*' || temp == '&')
 	{
 		return 1;
 	}
-	else{
+	else {
 		return 0;
 	}
 }
@@ -77,12 +84,12 @@ int getOperatorPrecedence(char temp)
 void caculate_operator(char TEMP)
 {
 	if (TEMP == '(')
-	{  
+	{
 		OperatorStack.push(TEMP);//右括号直接压栈
 	}
 	else if (TEMP == ')')
 	{
-		while ((OperatorStackTop=OperatorStack.top()) != '(')
+		while ((OperatorStackTop = OperatorStack.top()) != '(')
 		{
 			Treenode* OperatorNode = new Treenode;
 			if (OperatorStackTop == '*')//孩子只有一个，只需要一个运算符
@@ -108,7 +115,7 @@ void caculate_operator(char TEMP)
 	}
 	else //如果是运算符*&|
 	{
-		while ((!OperatorStack.empty())&&(getOperatorPrecedence(OperatorStack.top()) >= getOperatorPrecedence(TEMP)))//栈不为空且栈顶符号的优先级如果高需要先让栈顶出栈
+		while ((!OperatorStack.empty()) && (getOperatorPrecedence(OperatorStack.top()) >= getOperatorPrecedence(TEMP)))//栈不为空且栈顶符号的优先级如果高需要先让栈顶出栈
 		{
 			OperatorStackTop = OperatorStack.top();
 			Treenode* OperatorNode = new Treenode;
@@ -123,14 +130,14 @@ void caculate_operator(char TEMP)
 			else//需要两个运算符
 			{
 				OperatorNode->character = OperatorStackTop;
-				OperatorNode->rightchild= OperandsStack.top();//第一个出来的为右孩子
+				OperatorNode->rightchild = OperandsStack.top();//第一个出来的为右孩子
 				OperandsStack.pop();
 				OperatorNode->leftchild = OperandsStack.top();
 				OperandsStack.pop();
 				OperandsStack.push(OperatorNode);
 			}
 			OperatorStack.pop();//出栈
-			
+
 		}
 		OperatorStack.push(TEMP);
 	}
@@ -170,7 +177,7 @@ void BuildNFA(Tree root)
 		{
 			StartStatus->inputStatus1 = root->character;
 			StartStatus->Status1 = EndStatus;
-			StartStatus->inputStatus2 ='$';//这里只要占位即可，只要是看这个状态存不存在，存在才会看输入！！！
+			StartStatus->inputStatus2 = '$';//这里只要占位即可，只要是看这个状态存不存在，存在才会看输入！！！
 			StartStatus->Status2 = NULL;
 			//更新我的列表
 			TransformTable.push_back({ StartStatus,root->character,EndStatus });
@@ -252,14 +259,14 @@ void BuildNFA(Tree root)
 	}
 }
 vector<NFAnode*> CalculateClosure(vector<NFAnode*>node)//计算一个节点的闭包状态，这里默认节点必然是有的
-{	
+{
 	stack<NFAnode*>tempNodeStack;//存放临时状态
 	NFAnode* tempNode;
 	for (int i = 0; i < node.size(); i++)
 	{
 		tempNodeStack.push(node[i]);//node入栈
 	}
-	
+
 	while (!tempNodeStack.empty())//只要栈还不是空的
 	{
 		tempNode = tempNodeStack.top();//取
@@ -291,7 +298,7 @@ bool ifExistNotmarked(vector<StatusSetLine>StatusSet)
 	for (int i = 0; i < StatusSet.size(); i++)
 	{
 		if (StatusSet[i].flag == 0)
-		{   
+		{
 			StatusLine = StatusSet[i].Status;
 			StatusLineIndex = i;
 			return 1;
@@ -305,7 +312,7 @@ int isInStatusSet(vector<NFAnode*>StatusSet)
 	for (int i = 0; i < StatusSets.size(); i++)//在现有的状态机当中找是否存在一个和输入状态一样的状态集
 	{
 		StatusInSet = StatusSets[i].Status;
-		if(StatusSet.size()!= StatusInSet.size())//如果大小都不相等必然不是一个状态集
+		if (StatusSet.size() != StatusInSet.size())//如果大小都不相等必然不是一个状态集
 		{
 			continue;
 		}
@@ -339,6 +346,20 @@ bool IsFinal(vector<NFAnode*>StatusSet)
 	}
 	return 0;
 }
+#include <iostream>
+#include <vector>
+
+bool areArraysEqual(const std::vector<int>& array1, const std::vector<int>& array2) {
+
+	for (size_t i = 0; i < array1.size(); ++i) {
+		if (array1[i] != array2[i]) {
+			return false; // 如果有任何不匹配的元素，返回false
+		}
+	}
+
+	return true; // 如果没有不匹配的元素，返回true
+}
+
 void BuildDFA()
 {
 	while (ifExistNotmarked(StatusSets))//存在未标记的
@@ -370,37 +391,199 @@ void BuildDFA()
 				if (found == -1)
 				{
 					bool FinalLabel = IsFinal(ClosureStatus);
-					StatusSets.push_back({ ClosureStatus,0,FinalLabel});//加入一个新状态
+					StatusSets.push_back({ ClosureStatus,0,FinalLabel });//加入一个新状态
 					TransformTableDFA.push_back({ StatusLineIndex,input,int(StatusSets.size()) - 1 });//当前状态，输入字符，新加入的状态下标就是最后一行
 				}
 				else
 				{
 					TransformTableDFA.push_back({ StatusLineIndex,input,found });
 				}
-				
+
 			}
 		}
+	}
+}
+void MinDFA()
+{
+	vector<int> Final;
+	vector<int> NotFinal;
+	for (int i = 0; i < StatusSets.size(); i++)
+	{
+		if (StatusSets[i].isFinal == 1)
+		{
+			Final.push_back(i);
+		}
+		else
+		{
+			NotFinal.push_back(i);
+		}
+	}
+	DFAState.push_back(Final);//第一行放所有终态
+	DFAState.push_back(NotFinal);//第二行放初始化的所有非终态
+	//打印终态行和非终态行
+	for (int i = 0; i < DFAState.size(); i++)
+	{
+		vector<int>State = DFAState[i];
+		for (int j = 0; j < State.size(); j++)
+		{
+			cout << State[j] << " ";
+		}
+		cout << endl;
+	}
+	//开始判断非终态行是否可以再分
+	bool changed = true;
+	while (changed)
+	{
+		changed = false;
+		int SIZE = DFAState.size();
+		for (int index = 1; index < SIZE; index++)//合并可能的非终态,
+		{
+			if (DFAState[index].size() > 1)
+			{
+				vector<vector<int>>NextDFAStateS;
+				vector<int>State = DFAState[index];
+				vector<UpdateStatus>UPDATE;
+				//cout << "State.size():" << State.size();
+				for (int j = 0; j < State.size(); j++)//第一轮，逐个取非终态
+				{
+					vector<int>NextState;
+					for (char a : Operands)
+					{
+						int NextStatus = -1;//先假设找不到下一个状态
+						for (const TransformStateDFA& row : TransformTableDFA) {
+							if ((row.LastStatus == State[j]) && (row.InputCharacter == a))//DFA有唯一确定的下一个状态
+							{//找到下一状态当前的索引
+								for (int p = 0; p < DFAState.size(); p++)
+								{
+									for (int q = 0; q < DFAState[p].size(); q++)
+									{
+										if (row.NextStatus == DFAState[p][q])
+										{
+											NextStatus = p;
+											break;
+										}
+
+									}
+								}
+							}
+						}
+						NextState.push_back(NextStatus);
+					}
+					NextDFAStateS.push_back(NextState);
+					//cout << "NextDFAStateS.size():" << NextDFAStateS.size() << endl;
+					/*if (j == 0) {
+						vector<int>initStateIndex;
+						initStateIndex.push_back(0);
+						UPDATE.push_back({ NextDFAStateS[0],initStateIndex });
+					}*/
+
+					//尝试打印我的Update
+				}
+				vector<int>initStateIndex;
+				initStateIndex.push_back(0);
+				UPDATE.push_back({ NextDFAStateS[0],initStateIndex });
+				for (int i = 1; i < NextDFAStateS.size(); i++)
+				{
+					vector<int>State = NextDFAStateS[i];//得到一个状态
+					bool exist = 0;
+					for (int j = 0; j < UPDATE.size(); j++)
+					{
+						bool outcome = areArraysEqual(State, UPDATE[j].nextstatus);
+						if (outcome)
+						{
+							UPDATE[j].nowstatusindex.push_back(i);
+							exist = 1;
+							break;
+						}
+					}
+					if (!exist)//状态被更新了
+					{
+						vector<int>NewStateIndex;
+						NewStateIndex.push_back(i);
+						UPDATE.push_back({ NextDFAStateS[i],NewStateIndex });
+						changed = true;
+					}
+				}
+				//打印状态转化
+				for (int p = 0; p < NextDFAStateS.size(); p++)
+				{
+					for (int q = 0; q < NextDFAStateS[p].size(); q++)
+					{
+						cout << NextDFAStateS[p][q] << " ";
+					}
+					cout << endl;
+				}
+				//状态更新DFAState
+				for (int i = 0; i < UPDATE.size(); i++) {
+					for (int p = 0; p < UPDATE[i].nextstatus.size(); p++)
+					{
+						cout << UPDATE[i].nextstatus[p] << " ";
+					}
+					for (int p = 0; p < UPDATE[i].nowstatusindex.size(); p++)
+					{
+						cout << UPDATE[i].nowstatusindex[p] << " ";
+					}
+					cout << endl;
+				}
+				if (changed == true) {
+					for (int i = UPDATE.size() - 1; i >= 0; i--) {
+						if (i == 0)
+						{
+							vector<int>newDFAState;
+							for (int p = 0; p < UPDATE[i].nowstatusindex.size(); p++)
+							{
+								int low_index = UPDATE[i].nowstatusindex[p];
+
+								newDFAState.push_back(DFAState[index][low_index]);
+							}
+							DFAState[index] = newDFAState;
+
+						}
+						else {
+							vector<int>newDFAState;
+							for (int p = 0; p < UPDATE[i].nowstatusindex.size(); p++)
+							{
+								int low_index = UPDATE[i].nowstatusindex[p];
+								newDFAState.push_back(DFAState[index][low_index]);
+							}
+							DFAState.push_back(newDFAState);
+						}
+					}
+				}
+			}
+
+		}
+
+	}
+	for (int i = 0; i < DFAState.size(); i++)
+	{
+		vector<int>State = DFAState[i];
+		for (int j = 0; j < State.size(); j++)
+		{
+			cout << State[j] << " ";
+		}
+		cout << endl;
 	}
 }
 int main()
 {
 	string regex;
 	cin >> regex;
-	for (int i = 0; i < regex.length();i++)
+	for (int i = 0; i < regex.length(); i++)
 	{
 		TEMP = regex[i];
-		
+
 		if (isoperator(TEMP))//是一个运算符
 		{
 			if ((TEMP == '(') && (i != 0) && (regex[i - 1] != '|'))
 			{
-			caculate_operator('&');
-		}
+				caculate_operator('&');
+			}
 			caculate_operator(TEMP);
 		}
 		else
 		{
-			auto it = find(Operands.begin(), Operands.end(), TEMP );
+			auto it = find(Operands.begin(), Operands.end(), TEMP);
 			if (it == Operands.end())//没有这个字符
 			{
 				Operands.push_back(TEMP);
@@ -415,7 +598,7 @@ int main()
 				OperandsStack.push(OperandNode);
 			}
 			else
-			{    
+			{
 				if (regex[i - 1] == '(' || regex[i - 1] == '|')
 				{
 					;
@@ -431,7 +614,7 @@ int main()
 				OperandNode->leftchild = NULL;
 				OperandNode->rightchild = NULL;
 				OperandsStack.push(OperandNode);
-			}	
+			}
 		}
 	}
 	while (!OperatorStack.empty())//非空，需要清空符号栈
@@ -467,10 +650,10 @@ int main()
 	cout << "TransformTable has " << TransformTable.size() << " rows." << endl;
 
 	//NFA已经建立完毕，我们可以在表中得到所有边的信息，现在我们要将NFA->DFA,从NFA栈中取出开始状态
-    //step1:先确定初始状态，此时必然是可以找到的所以没有设置找不到返回的默认值
+	//step1:先确定初始状态，此时必然是可以找到的所以没有设置找不到返回的默认值
 	vector<NFAnode*>Status;
 	Status.push_back(StatusStack.top().first);//本身已经被包含其中
-	vector<NFAnode*>ClosureStatus=CalculateClosure(Status);
+	vector<NFAnode*>ClosureStatus = CalculateClosure(Status);
 	bool FinalLabel = IsFinal(ClosureStatus);
 	StatusSets.push_back({ ClosureStatus,0,FinalLabel });//加入一个新状态
 	//在有了初状态的前提下开始建立DFA
@@ -481,11 +664,12 @@ int main()
 		{
 			cout << row.Status[i] << " ";
 		}
-		cout <<row.isFinal<<endl;
+		cout << row.isFinal << endl;
 	}
 	//打印状态集的转化关系
 	for (const TransformStateDFA& row : TransformTableDFA) {
 		cout << row.LastStatus << "\t" << row.InputCharacter << "\t" << row.NextStatus << endl;
 	}
-	return 0;	
+	MinDFA();
+	return 0;
 }
